f81174242c9d3fc025ba39ed8ea0d41ea3e3ea17
Thibault Farges <farges.thibault@gmail.com>
2024-05-19 21:27:42 +1200

    Comment out google bench test broken include file

--- src/common/test_bench.cpp
+++ src/common/test_bench.cpp
@@ -1,36 +1,36 @@
-#include <benchmark/benchmark.h>
+// #include <benchmark/benchmark.h>
 
-class MyBenchmark {
- public:
-  static int addition(int a, int b) { return a + b; }
+// class MyBenchmark {
+//  public:
+//   static int addition(int a, int b) { return a + b; }
 
-  static int subtraction(int a, int b) { return a - b; }
-};
+//   static int subtraction(int a, int b) { return a - b; }
+// };
 
-// Fonction de benchmark pour l'addition
-static void BM_Addition(benchmark::State& state) {
-  // Récupération des arguments passés à la fonction de benchmark
-  int a = state.range(0);
-  int b = state.range(1);
+// // Fonction de benchmark pour l'addition
+// static void BM_Addition(benchmark::State& state) {
+//   // Récupération des arguments passés à la fonction de benchmark
+//   int a = state.range(0);
+//   int b = state.range(1);
 
-  for (auto _ : state) {
-    int result = MyBenchmark::addition(a, b);
-    benchmark::DoNotOptimize(result);
-  }
-}
-BENCHMARK(BM_Addition)->Args({10, 20})->Args({100, 200});
+//   for (auto _ : state) {
+//     int result = MyBenchmark::addition(a, b);
+//     benchmark::DoNotOptimize(result);
+//   }
+// }
+// BENCHMARK(BM_Addition)->Args({10, 20})->Args({100, 200});
 
-// Fonction de benchmark pour la soustraction
-static void BM_Subtraction(benchmark::State& state) {
-  // Récupération des arguments passés à la fonction de benchmark
-  int a = state.range(0);
-  int b = state.range(1);
+// // Fonction de benchmark pour la soustraction
+// static void BM_Subtraction(benchmark::State& state) {
+//   // Récupération des arguments passés à la fonction de benchmark
+//   int a = state.range(0);
+//   int b = state.range(1);
 
-  for (auto _ : state) {
-    int result = MyBenchmark::subtraction(a, b);
-    benchmark::DoNotOptimize(result);
-  }
-}
-BENCHMARK(BM_Subtraction)->Args({10, 5})->Args({100, 50});
+//   for (auto _ : state) {
+//     int result = MyBenchmark::subtraction(a, b);
+//     benchmark::DoNotOptimize(result);
+//   }
+// }
+// BENCHMARK(BM_Subtraction)->Args({10, 5})->Args({100, 50});
 
-BENCHMARK_MAIN();
+// BENCHMARK_MAIN();
